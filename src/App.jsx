e08a42d4f71f969716123d5bb6dd61d2959867bc
LindaTancini <lindatancini95@gmail.com>
2025-05-27 16:18:45 +0200

    feat: change Homepage folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import DefaultLayout from './layout/DefaultLayout'
-import ProductListPage from './pages/ProductListPage'
-import CartPage from './pages/CartPage'
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import DefaultLayout from "./layout/DefaultLayout";
+import ProductListPage from "./pages/ProductListPage";
+import CartPage from "./pages/CartPage";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<DefaultLayout> <HomePage /> </DefaultLayout>} />
-                <Route path="/products" element={<DefaultLayout><ProductListPage /></DefaultLayout>} />
-                <Route path="/cart" element={<DefaultLayout><CartPage /></DefaultLayout>} />
-
-            </Routes>
-        </Router>
-    )
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <DefaultLayout>
+              {" "}
+              <HomePage />{" "}
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path="/products"
+          element={
+            <DefaultLayout>
+              <ProductListPage />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <DefaultLayout>
+              <CartPage />
+            </DefaultLayout>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
