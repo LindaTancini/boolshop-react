026c5c0762214f2173bda8721b099cb25fec47bc
LindaTancini <lindatancini95@gmail.com>
2025-05-28 18:20:23 +0200

    feat: add albums

--- src/components/CarouselBis.jsx
+++ src/components/CarouselBis.jsx
@@ -1,29 +1,59 @@
-function CarouselBis(){
-    return(
-        <div className="container">
-            <div id="carouselExample-bis" className="carousel slide">
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGh5WFH8TOIfRKxUrIgJZoDCs1yvQ4hIcppw&s" className="d-block mx-auto" alt="..."/>
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGh5WFH8TOIfRKxUrIgJZoDCs1yvQ4hIcppw&s" className="d-block mx-auto" alt="..."/>
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGh5WFH8TOIfRKxUrIgJZoDCs1yvQ4hIcppw&s" className="d-block mx-auto" alt="..."/>
-                    </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample-bis" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExample-bis" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
+function CarouselBis({ albums }) {
+  return (
+    <div className="container my-5">
+      <div
+        id="carouselExample"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          {albums.map((album, index) => (
+            <div
+              key={album.id}
+              className={`carousel-item ${index === 0 ? "active" : ""}`}
+            >
+              <div className="d-flex justify-content-center">
+                <img
+                  src={album.imagePath}
+                  className="d-block w-100 mx-auto"
+                  alt={album.title}
+                />
+              </div>
+              <div className="text-black  p-3 mt-2 rounded w-100 text-center">
+                <h5>{album.title}</h5>
+                <p>{album.price} €</p>
+              </div>
             </div>
+          ))}
         </div>
-    )
+
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default CarouselBis
+export default CarouselBis;
