6dfdcf705dbc67be84ea25957898487afcd72108
LindaTancini <lindatancini95@gmail.com>
2025-05-28 18:20:58 +0200

    feat: add props

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,70 +6,47 @@ import ContextLoader from "../contexts/contextLoader";
 import ContextError from "../contexts/contextError";
 
 function HomePage() {
-    const [albums, setAlbums] = useState([]);
-    const api = "http://127.0.0.1:3000/api/album";
+  const [albums, setAlbums] = useState([]);
+  const api = "http://127.0.0.1:3000/api/album";
 
-    const filterAlbumsLowCost = albums.filter(album => parseFloat(album.price) <= 20);
-    const filterAlbumsMoreBuy = albums.filter(album => Date.parse(album.date) < Date.now());
-    console.log("Costo più basso: ", filterAlbumsLowCost);
-    console.log("Più recenti: ", filterAlbumsMoreBuy);
+  const filterAlbumsLowCost = albums.filter(
+    (album) => parseFloat(album.price) <= 20
+  );
+  const filterAlbumsMoreBuy = albums.filter(
+    (album) => Date.parse(album.date) < Date.now()
+  );
+  console.log("Costo più basso: ", filterAlbumsLowCost);
+  console.log("Più recenti: ", filterAlbumsMoreBuy);
 
-    const { setIsLoading } = useContext(ContextLoader);
-    const { setIsError } = useContext(ContextError);
+  const { setIsLoading } = useContext(ContextLoader);
+  const { setIsError } = useContext(ContextError);
 
-    function getAlbums() {
-        setIsLoading(true);
-        axios.get(api)
-            .then(res => {
-                setAlbums(res.data);
+  function getAlbums() {
+    setIsLoading(true);
+    axios
+      .get(api)
+      .then((res) => {
+        setAlbums(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
 
-            })
-            .catch(err => {
-                console.log(err);
-                setIsError(true);
-            })
-            .finally(() => {
-                setIsLoading(false);
-            });
-    }
-
-    useEffect(getAlbums, []);
-    console.log(albums);
-    return (
-        <>
-            <div>
-                <h2>Album meno costosi</h2>
-                <ul>
-                    {filterAlbumsLowCost.map((album) => (<li key={album.id}>
-                        <p>title: {album.title}</p>
-                        <p>price: {album.price}€</p>
-                        <p>quantity: {album.quantity}</p>
-                        <img src={album.imagePath} alt={album.title} />
-                        <p>release date: {album.date.substring(0, 10)}</p>
-                    </li>
-                    ))}
-                </ul>
-            </div>
-            <div>
-                <h2>Album più recenti</h2>
-                <ul>
-                    {filterAlbumsMoreBuy.map((album) => (<li key={album.id}>
-                        <p>title: {album.title}</p>
-                        <p>price: {album.price}€</p>
-                        <p>quantity: {album.quantity}</p>
-                        <p>release date: {album.date.substring(0, 10)}</p>
-                    </li>
-                    ))}
-                </ul>
-            </div>
-
-            <h2 className="text-center">Gli album più venduti</h2>
-            <Carousel albums={albums} />
-            <h2 className="text-center">Gli ultimi arrivi</h2>
-            <CarouselBis />
-        </>
-
-    )
+  useEffect(getAlbums, []);
+  console.log(albums);
+  return (
+    <>
+      <h2 className="text-center">Gli album più venduti</h2>
+      <Carousel albums={albums} />
+      <h2 className="text-center">Gli ultimi arrivi</h2>
+      <CarouselBis albums={albums} />
+    </>
+  );
 }
 
 export default HomePage;
