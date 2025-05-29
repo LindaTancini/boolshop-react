15d3bee0931091e78a3e2f22cb3108ddf2bcf24b
LindaTancini <lindatancini95@gmail.com>
2025-05-29 11:46:27 +0200

    feat: add albumdetails

--- src/pages/AlbumDetails.jsx
+++ src/pages/AlbumDetails.jsx
@@ -1,12 +1,88 @@
 //Importazioni
 import { useParams } from "react-router-dom";
+import ContextLoader from "../contexts/contextLoader";
+import ContextError from "../contexts/contextError";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
 
 function AlbumDetails() {
-  const { id } = useParams();
+  const { slug } = useParams();
+  const [album, setAlbum] = useState(null);
+  const api = `http://127.0.0.1:3000/api/album/${slug}`;
+  const { setIsLoading } = useContext(ContextLoader);
+  const { setIsError } = useContext(ContextError);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(api)
+      .then((res) => {
+        console.log(res.data);
+        setAlbum(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        setIsError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [slug]);
+
+  if (!album) {
+    return <p>Caricamento...</p>;
+  }
 
   return (
     <div className="container mt-5">
-      <h2>Dettagli Album : {id}</h2>
+      <h2 className="mb-4 text-center fw-bold">{album.title}</h2>
+
+      <div className="d-flex flex-column flex-md-row align-items-center justify-content-center gap-4 text-center p-4 rounded shadow-lg bg-album-details-shadow">
+        {album.imagePath && (
+          <img
+            src={album.imagePath}
+            alt={album.title}
+            className="img-fluid rounded shadow-sm"
+          />
+        )}
+
+        <div className="text-center text-md-start">
+          <p className="fs-5 mb-3">
+            <strong>Prezzo:</strong> {album.price} €
+          </p>
+          <p className="fs-5 mb-3">
+            <strong>Artista:</strong> {album.artist.name}
+          </p>
+          <p className="fs-5 mb-3">
+            <strong>Genere:</strong> {album.genre.name}
+          </p>
+          <p className="fs-5 mb-3">
+            <strong>Quantità disponibile:</strong> {album.quantity}
+          </p>
+          <p className="fs-5 mb-3">
+            <strong>Formato:</strong> {album.format}
+          </p>
+          <p className="fs-5 mb-4">
+            <strong>Data di rilascio:</strong> {album.date}
+          </p>
+
+          <div className="d-flex justify-content-center  gap-3">
+            <button
+              type="button"
+              title="Aggiungi al carrello"
+              className="btn btn-outline-secondary"
+            >
+              Aggiungi al carrello
+            </button>
+            <button
+              className="btn btn-outline-danger rounded-circle wishlist-button"
+              title="Aggiungi alla wishlist"
+            >
+              <i className="fas fa-heart"></i>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
