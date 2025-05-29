85f8bb243cb4ef06c15acb1821549ea9b7303dc5
LindaTancini <lindatancini95@gmail.com>
2025-05-29 12:31:48 +0200

    feat: create genre page

--- src/pages/GenresPage.jsx
+++ src/pages/GenresPage.jsx
@@ -1,7 +1,55 @@
-const GenresPage = () => {
-    return <>
+//Importazioni
+import { useContext, useEffect, useState } from "react";
+import ContextLoader from "../contexts/contextLoader";
+import ContextError from "../contexts/contextError";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function GenresPage() {
+  const [genre, setGenre] = useState([]);
+  const api = "http://127.0.0.1:3000/api/genres";
+  const { setIsLoading } = useContext(ContextLoader);
+  const { setIsError } = useContext(ContextError);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(api)
+      .then((res) => {
+        console.log(res.data);
+        setGenre(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        setIsError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>
         <h3>Generi musicali:</h3>
+        <ul className="list-group">
+          {genre.map((element) => (
+            <li
+              key={element.id}
+              className="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <Link
+                to={`/genres/${element.slug}`}
+                className="text-decoration-none text-primary fw-bold"
+              >
+                {e.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
+  );
 }
 
 export default GenresPage;
