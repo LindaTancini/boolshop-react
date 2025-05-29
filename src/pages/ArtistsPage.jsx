2ab1587cfaafdb3f20b798a0486739860a0a9217
LindaTancini <lindatancini95@gmail.com>
2025-05-29 18:57:00 +0200

    feat: change button artist

--- src/pages/ArtistsPage.jsx
+++ src/pages/ArtistsPage.jsx
@@ -5,74 +5,80 @@ import ContextLoader from "../contexts/contextLoader";
 import ContextError from "../contexts/contextError";
 
 const ArtistsPage = () => {
-    const [artists, setArtists] = useState([]);
-    const [search, setSearch] = useState([]);
+  const [artists, setArtists] = useState([]);
+  const [search, setSearch] = useState([]);
 
-    const api = "http://127.0.0.1:3000/api/artists";
-    const { setIsLoading } = useContext(ContextLoader);
-    const { setIsError } = useContext(ContextError);
+  const api = "http://127.0.0.1:3000/api/artists";
+  const { setIsLoading } = useContext(ContextLoader);
+  const { setIsError } = useContext(ContextError);
 
-    function searchArtist(e) {
-        e.preventDefault();
-        getArtists();
-    }
+  function searchArtist(e) {
+    e.preventDefault();
+    getArtists();
+  }
 
-    function getArtists(){
-        setIsLoading(true);
-        setIsError(false);
+  function getArtists() {
+    setIsLoading(true);
+    setIsError(false);
 
-        axios
-            .get(api, {
-                params: {
-                    search
-                }
-            })
-            .then((res) => {
-                setArtists(res.data);
-            })
-            .catch((err) => {
-                console.error(err);
-                setIsError(true);
-            })
-            .finally(() => {
-                setIsLoading(false);
-            });     
-    }
+    axios
+      .get(api, {
+        params: {
+          search,
+        },
+      })
+      .then((res) => {
+        setArtists(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        setIsError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
 
-    useEffect(getArtists, [search]);
+  useEffect(getArtists, [search]);
 
-    // if (!artists.length) return <p>Caricamento...</p>;
+  // if (!artists.length) return <p>Caricamento...</p>;
 
-    return (
-
-        <div>
-            <form onSubmit={searchArtist}>
-                <label htmlFor="searchAlbum" />
-                <input type="text"
-                    onChange={(e) => setSearch(e.target.value)}
-                    value={search}
-                    placeholder="album to search..."
-                />
-                <button className="btn btn-outline-danger mx-2" type="submit">Search</button>
-            </form>
-            <h1>Elenco Artisti</h1>
-            <ul className="list-group">
-                {artists.map((artist) => (
-                    <li
-                        key={artist.id}
-                        className="list-group-item d-flex justify-content-between align-items-center"
-                    >
-                        <Link
-                            to={`/artists/${artist.slug}`}
-                            className="text-decoration-none text-primary fw-bold"
-                        >
-                            {artist.name}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+  return (
+    <div>
+      <form onSubmit={searchArtist} className="mb-4 d-flex justify-content-end">
+        <div className="input-group w-50">
+          <input
+            id="searchAlbum"
+            type="text"
+            className="form-control"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+            placeholder="Cerca un Artista..."
+            aria-label="Search Artist"
+          />
+          <button className="btn btn-outline-danger" type="submit">
+            Cerca
+          </button>
         </div>
-    );
+      </form>
+      <h1>Elenco Artisti</h1>
+      <ul className="list-group">
+        {artists.map((artist) => (
+          <li
+            key={artist.id}
+            className="list-group-item d-flex justify-content-between align-items-center"
+          >
+            <Link
+              to={`/artists/${artist.slug}`}
+              className="text-decoration-none text-primary fw-bold"
+            >
+              {artist.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ArtistsPage;
