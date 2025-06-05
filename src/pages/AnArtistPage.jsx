cd7a9aef8b9b30388ffc38bb06c1baa3d6379c1d
vmqxdv <vquintilio@gmail.com>
2025-06-05 18:50:35 +0200

    reafactor: passato da 4 a 2 spazi

--- src/pages/AnArtistPage.jsx
+++ src/pages/AnArtistPage.jsx
@@ -5,67 +5,67 @@ import axios from 'axios';
 
 
 const AnArtistPage = () => {
-    const navigate = useNavigate();
-    const { slug } = useParams();
-    const [artist, setArtist] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const { slug } = useParams();
+  const [artist, setArtist] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        if (slug) {
-            axios.get(`http://127.0.0.1:3000/api/artists/${slug}`)
-                .then(response => {
-                    setArtist(response.data);
-                    setLoading(false);
-                })
-                .catch(err => {
-                    if (err.response) {
-                        setError(`Errore: ${err.response.status} - ${err.response.data.message || 'Errore del server'}`);
-                    } else if (err.request) {
-                        setError("Errore di rete: Nessuna risposta dal server.");
-                    } else {
-                        setError(`Errore: ${err.message}`);
-                    }
-                    setLoading(false);
-                });
-        }
-    }, [slug]);
-
-    if (loading) {
-        return <div>Caricamento informazioni artista</div>;
+  useEffect(() => {
+    if (slug) {
+      axios.get(`http://127.0.0.1:3000/api/artists/${slug}`)
+        .then(response => {
+          setArtist(response.data);
+          setLoading(false);
+        })
+        .catch(err => {
+          if (err.response) {
+            setError(`Errore: ${err.response.status} - ${err.response.data.message || 'Errore del server'}`);
+          } else if (err.request) {
+            setError("Errore di rete: Nessuna risposta dal server.");
+          } else {
+            setError(`Errore: ${err.message}`);
+          }
+          setLoading(false);
+        });
     }
+  }, [slug]);
 
-    if (error) {
-        return <div className="alert alert-danger" role="alert">{error}</div>;
-    }
+  if (loading) {
+    return <div>Caricamento informazioni artista</div>;
+  }
 
-    // Formattazione della data di nascita
-    const birthDate = artist.birth_date ? new Date(artist.birth_date).toLocaleDateString() : 'N/D';
+  if (error) {
+    return <div className="alert alert-danger" role="alert">{error}</div>;
+  }
 
-    const imageUrl = artist.image_url ? `http://127.0.0.1:3000/${artist.image_url}` : '';
+  // Formattazione della data di nascita
+  const birthDate = artist.birth_date ? new Date(artist.birth_date).toLocaleDateString() : 'N/D';
 
-    return (
-        <div className="container mt-4">
-            <h1 className="mb-4 fw-bold text-orange text-shadow-orange">{artist.name}</h1>
-            <div className="custom-artist-card card mb-4">
-                <div className="card-body">
-                    <h5 className="card-title">Informazioni Artista</h5>
-                    <img src={imageUrl} className="img-fluid mb-3" alt={artist.name} />
-                    <p className="card-text">
-                        <strong>Data di Nascita:</strong> {birthDate}
-                    </p>
-                    <p className="card-text">
-                        <strong>Biografia:</strong> {artist.biography}
-                    </p>
-                </div>
-            </div>
-            <button
-                className="button-orange-outline"
-                onClick={() => navigate("/artists")}
-            >Torna all'elenco artisti
-            </button>
+  const imageUrl = artist.image_url ? `http://127.0.0.1:3000/${artist.image_url}` : '';
+
+  return (
+    <div className="container mt-4">
+      <h1 className="mb-4 fw-bold text-orange text-shadow-orange">{artist.name}</h1>
+      <div className="custom-artist-card card mb-4">
+        <div className="card-body">
+          <h5 className="card-title">Informazioni Artista</h5>
+          <img src={imageUrl} className="img-fluid mb-3" alt={artist.name} />
+          <p className="card-text">
+            <strong>Data di Nascita:</strong> {birthDate}
+          </p>
+          <p className="card-text">
+            <strong>Biografia:</strong> {artist.biography}
+          </p>
         </div>
-    );
+      </div>
+      <button
+        className="button-orange-outline"
+        onClick={() => navigate("/artists")}
+      >Torna all'elenco artisti
+      </button>
+    </div>
+  );
 };
 
 export default AnArtistPage;
