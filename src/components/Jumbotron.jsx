b64eed3f128e558b21bc70be2a0619e7d47db6d4
LindaTancini <lindatancini95@gmail.com>
2025-06-05 17:20:38 +0200

    style: add margin top

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -13,7 +13,7 @@ function Jumbotron() {
         </p>
       </div>
       <div className="jumbotron-modify text-white d-flex flex-column justify-content-center align-items-center text-center">
-        <div className="container py-0">
+        <div className="container py-0 mt-5">
           <h1 className="display-4 fw-bold">Benvenuto nel nostro negozio !</h1>
           <p className="lead">
             Scopri le ultime novità, i più venduti e approfitta delle nostre
